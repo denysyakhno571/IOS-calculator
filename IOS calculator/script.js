29da7a233126f3bdc1e2e6b977358b9c9f9a7c24
denysyakhno571 <125214795+denysyakhno571@users.noreply.github.com>
2023-02-12 21:30:58 +0200

    Add files via upload

---
+++ IOS calculator/script.js
@@ -0,0 +1,87 @@
+const numbers = document.querySelectorAll('.numbers');
+const result = document.querySelector('.result span');
+const signs = document.querySelectorAll('.sign');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('clear');
+const negative = document.querySelector('negative');
+const present = document.querySelector('present');
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let sign = "";
+let resultValue = 0;
+
+for(let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', (e) => {
+        let atr = e.target.getAttribute('value');
+        if(isFirstValue === false) {
+            getFirstValue(atr)
+        }
+        if (isSecondValue === false) {
+            detSecondValue(atr)
+        }
+    })
+}
+
+function getFirstValue (el) {
+    result.innerHTML = "";
+    firstValue += el;
+    result.innerHTML = firstValue;
+    firstValue = + firstValue;
+}
+
+function detSecondValue (el) {
+    if (firstValue !== '' && sign !== '') {
+        secondValue += el;
+        result.innerHTML = secondValue;
+        secondValue = + secondValue;
+    }
+}
+
+function getSign () {
+    for(let i = 0; i < signs.length; i++) {
+        signs[i].addEventListener('click', (e) => {
+            sign = e.target.getAttribute('value');
+            isFirstValue = true;
+        })
+    }
+}
+ getSign();
+
+ equals.addEventListener('click', () => {
+    result.innerHTML = "";
+    if (sign === '+') {
+        resultValue = firstValue + secondValue;
+    } else  if (sign === '-') {
+        resultValue = firstValue - secondValue;
+    } else  if (sign === 'X') {
+        resultValue = firstValue * secondValue;
+    } else  if (sign === '/') {
+        resultValue = firstValue / secondValue;
+    } 
+    result.innerHTML = resultValue;
+    firstValue = resultValue;
+    secondValue = '';
+
+    checkResultLenght();
+
+ })
+
+ function checkResultLenght () {
+    resultValue = JSON.stringify(resultValue);
+    if (resultValue.length >= 8 ) {
+        resultValue = JSON.parse(resultValue);
+        result.innerHTML = resultValue.toFixed(5);
+    }
+ }
+
+negative.addEventListener('click', () => {
+    result.innerHTML = "";
+    if (firstValue != '') {
+        resultValue = -firstValue;
+        firstValue = resultValue;
+    }
+    result.innerHTML = resultValue;
+})
